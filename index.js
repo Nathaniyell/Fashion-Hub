3a669f3c14461cea0d6199c68cf816cff42dbae2
Nathaniyell <essien.nathan@yahoo.com>
2023-11-21 15:18:03 +0100

    update index.html and index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const cart = document.getElementById("");
-const price = document.getElementById("price")
+const price = document.getElementById("clothPrice")
+const title = document.getElementById("clothTitle")
 let cart = [];
 function displayCart(a) {
   let i = 0;
