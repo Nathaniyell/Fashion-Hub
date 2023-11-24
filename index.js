4d4de23d4b3162f2c65d4200c77720d88f7c532c
Nathaniyell <essien.nathan@yahoo.com>
2023-11-24 06:49:26 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // const cart = document.getElementById("");
-const price = document.getElementById("clothPrice");
-const title = document.getElementById("clothTitle");
 const cartItems = document.getElementById("cartItem");
 const addToCartBtn = document.getElementById("addToCart")
 let cart = [];
 addToCartBtn.addEventListener("click",()=>{
   const grandParent = addToCartBtn.parentElement.parentElement
-  const title = grandParent.querySelector("h1")
-  console.log(querySelector("h1").textContent)
+  const title = grandParent.querySelector("h1").textContent
+  const price = grandParent.querySelector("p").textContent
+ 
 
 
 })
