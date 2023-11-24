8b4e6e68beeec9d7fe7b6052f928fae1f5c62d76
Nathaniyell <essien.nathan@yahoo.com>
2023-11-24 06:29:58 +0100

    update index.js

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 const price = document.getElementById("clothPrice");
 const title = document.getElementById("clothTitle");
 const cartItems = document.getElementsById("cartItem");
+const addToCartBtn = document.getElementById("addToCart")
 let cart = [];
+function addToCart(){
+  cart.push()
+}
+
 function displayCart(a) {
   let i = 0;
   if (cart.length === 0) {
     cartItems.innerHTML = "Your Cart is empty";
   } else {
+    addToCartBtn.addEventListener("click",()=>{
+
+    })
     cartItems.innerHTML = `<div>
     
     </div>`;
