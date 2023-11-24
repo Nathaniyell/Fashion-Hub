0ae421e09a5953e43469d8927c86bd26457ec1a5
Nathaniyell <essien.nathan@yahoo.com>
2023-11-24 06:47:52 +0100

    update index.js

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ const title = document.getElementById("clothTitle");
 const cartItems = document.getElementById("cartItem");
 const addToCartBtn = document.getElementById("addToCart")
 let cart = [];
-function addToCart(){
-  // cart.push()
-  displayCart()
-}
+addToCartBtn.addEventListener("click",()=>{
+  const grandParent = addToCartBtn.parentElement.parentElement
+  const title = grandParent.querySelector("h1")
+  console.log(querySelector("h1").textContent)
 
-function displayCart(a) {
-  let i = 0;
-  if (cart.length === 0) {
-    cartItems.innerHTML = "Your Cart is empty";
-  } else {
-    addToCartBtn.addEventListener("click",()=>{
-      i++
-    })
-    console.log(addToCartBtn.parentNode)
-    cartItems.innerHTML = `<div>
+
+})
+// function addToCart(){
+//    cart.push()
+//   displayCart()
+// }
+
+// function displayCart(a) {
+//   let i = 0;
+//   if (cart.length === 0) {
+//     cartItems.innerHTML = "Your Cart is empty";
+//   } else {
+//     addToCartBtn.addEventListener("click",()=>{
+//       i++
+//     })
+//     console.log(addToCartBtn.parentNode)
+//     cartItems.innerHTML = `<div>
     
-    </div>`;
-  }
-}
+//     </div>`;
+//   }
+// }
