d0875725bd0201e085614085e000610b5bc05c28
Nathaniyell <essien.nathan@yahoo.com>
2023-11-24 07:21:00 +0100

    update index.html and index.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 // const cart = document.getElementById("");
 const cartItems = document.getElementById("cartItem");
-const addToCartBtn = document.getElementById("addToCart")
+const addToCartBtn = document.querySelectorAll("addToCart")
 let cart = [];
-addToCartBtn.addEventListener("click",()=>{
-  const parent = addToCartBtn.parentElement
-  const title = parent.parentElement.querySelector("h1").textContent
-  const price = parent.querySelector("#clothPrice").textContent
- cart.push({...cart,price, title})
-console.log(cart);
-if (cart.length===0){
-  cartItems.innerHTML ="Your cart is empty"
-}else{
-
-  cartItems.innerHTML= cart.map(item=>{
-    return(
-      `<div class="d-flex w-100 justify-content-between align-items-center">
-      <h4 class="fs-5">${item.title}</h4>
-      <p class="fs-5">${item.price}</p>
-      </div>
-      `
-    )
+addToCartBtn.forEach(btn=>{
+  btn.addEventListener('click',()=>{
+    const parent = btn.parentElement
+    const title = parent.parentElement.querySelector("h1").textContent
+    const price = parent.querySelector("#clothPrice").textContent
+   cart.push({...cart,price, title})
+  console.log(cart);
   })
-  
-
-}
 })
+
+function displayCart(){
+  if (cart.length===0){
+    cartItems.innerHTML ="Your cart is empty"
+  }else{
+  
+    cartItems.innerHTML= cart.map(item=>{
+      return(
+        `<div class="d-flex w-100 justify-content-between align-items-center">
+        <h4 class="fs-5">${item.title}</h4>
+        <p class="fs-5">${item.price}</p>
+        </div>
+        `
+      )
+    })
+    
+  
+  }
+}
+
+
 // function addToCart(){
 //    cart.push()
 //   displayCart()
