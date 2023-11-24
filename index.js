c23ab5b75e7cb93d9362a31fd9d10f6bb5ceba67
Nathaniyell <essien.nathan@yahoo.com>
2023-11-24 07:35:58 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 const cartItems = document.getElementById("cartItem");
-const addToCartBtn = document.querySelectorAll("#addToCart")
+const addToCartBtn = document.querySelectorAll("#addToCart");
 let cart = [];
-addToCartBtn.forEach(btn=>{
-  btn.addEventListener('click',()=>{
-    const parent = btn.parentElement
-    const title = parent.parentElement.querySelector("h1").textContent
-    const price = parent.querySelector("#clothPrice").textContent
-   cart.push({price, title})
-  console.log(cart);
-  })
- displayCart()
-})
+addToCartBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const parent = btn.parentElement;
+    const title = parent.parentElement.querySelector("h1").textContent;
+    const price = parent.querySelector("#clothPrice").textContent;
+    cart.push({ price, title });
+    console.log(cart);
+  });
+  displayCart();
+});
 
-
-
-function displayCart(){
-  if (cart.length===0){
-    cartItems.innerHTML ="Your cart is empty"
-  }else{
-      cartItems.innerHTML= cart.map(item=>{
-      return(
-        `<div class="d-flex w-100 justify-content-between align-items-center">
+function displayCart() {
+  if (cart.length === 0) {
+    cartItems.innerHTML = "Your cart is empty";
+  } else {
+    cartItems.innerHTML = cart
+      .map((item) => {
+        return `<div class="d-flex w-100 justify-content-between align-items-center">
         <h4 class="fs-5">${item.title}</h4>
         <p class="fs-5">${item.price}</p>
         </div>
-        `
-      )
-    })
-    
-  
+        `;
+      })
+      .join("");
   }
 }
-
