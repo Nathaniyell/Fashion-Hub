c4d36c3ae6da5fdbbb5577125c60adeb590d77af
Nathaniyell <essien.nathan@yahoo.com>
2023-11-25 19:27:28 +0100

    update index.js

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ addToCartBtn.forEach(btn => {
   });
 });
 
-function deleteItem(title){
-cart.filter(item=>item.title !==title)
-displayCart()
-}
+// `splice`: This method modifies the original array by adding or removing elements. It's useful when you want to modify the existing array in place. The `splice` method changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+
+
+  function deleteItem(index) {
+    cart.splice(index, 1);
+    displayCart();
+  }
+
+
 
 function displayCart() {
   if (cart.length === 0) {
@@ -26,12 +31,12 @@ function displayCart() {
     cartItems.innerHTML = "Your cart is empty";
   } else {
     // If there are items in the cart, display each item
-    cartItems.innerHTML = cart.map(item => {
+    cartItems.innerHTML = cart.map((item, index) => {
       return (
         `<div class="d-flex w-100 justify-content-between align-items-center">
           <h4 class="fs-5">${item.title}</h4>
           <p class="fs-5">${item.price}</p>
-          <button type="button" onclick="deleteItem(${item.title})"><i class="fa-solid fa-trash"></i></button>
+          <button type="button" onclick="deleteItem(${index})"><i class="fa-solid fa-trash"></i></button>
         </div>`
       );
     }).join(''); // Use join('') to convert the array of strings to a single string
