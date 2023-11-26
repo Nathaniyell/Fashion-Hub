936c36257df7d612145dd699770fdbd00932370e
Nathaniyell <essien.nathan@yahoo.com>
2023-11-26 17:01:35 +0100

    update index.html, index.js and main.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const addToCartBtn = document.querySelectorAll("#addToCart");
 const cartCount = document.getElementById("count")
 const cartTotal = document.getElementById("total")
 const cartIcon = document.querySelector(".nav__Cart")
-
 let cart = [];
 
 cartIcon.addEventListener("click", ()=>{
@@ -15,8 +14,8 @@ addToCartBtn.forEach(btn => {
     const parent = btn.parentElement;
     const title = parent.parentElement.querySelector("h1").textContent;
     const price = parent.querySelector("#clothPrice").textContent;
-    cart.push({price, title });
-    localStorage.setItem("cartItems", JSON.stringify(cart))
+    cart.push({price:price, title });
+  
     console.log(cart);
     console.log("Your Item has been added to cart")
 
@@ -51,8 +50,7 @@ function displayCart() {
   } else {
     let total = 0
     // If there are items in the cart, display each item
-    
-    cartItems.innerHTML = StoredItems.map((item, index) => {
+    cartItems.innerHTML = cart.map((item, index) => {
       let {title,price} = item
       total +=parseFloat(price)
       cartTotal.innerHTML = "N "+total+" .00"
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const cartItems = document.getElementById("cartItem");
 const addToCartBtn = document.querySelectorAll("#addToCart");
-const cartCount = document.getElementById("count");
+const cartCountElement = document.getElementById("count");
 const cartTotal = document.getElementById("total");
 const cartIcon = document.querySelector(".nav__Cart");
 const StoredItems = JSON.parse(localStorage.getItem("cartItems")) || []; // Ensure StoredItems is initialized as an array
+
 let cart = StoredItems; // Initialize cart with stored items
+let cartCount = StoredItems.length
 
 cartIcon.addEventListener("click", () => {
   cartItems.parentElement.classList.toggle("show-cart");
@@ -14,41 +16,74 @@ addToCartBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     const parent = btn.parentElement;
     const title = parent.parentElement.querySelector("h1").textContent;
-    const price = parent.querySelector("#clothPrice").textContent;
-    cart.push({ price, title });
+    const price = parseFloat(parent.querySelector("#clothPrice").textContent); // Convert price to a floating-point number
+
+    // Check if the item already exists in the cart
+    const existingItemIndex = cart.findIndex((item) => item.title === title);
+
+    if (existingItemIndex !== -1) {
+      // If the item exists, update the quantity and total price
+      cart[existingItemIndex].quantity += 1;
+      cart[existingItemIndex].totalPrice = cart[existingItemIndex].quantity * price;
+    } else {
+      // If the item doesn't exist, add it to the cart
+      cart.push({ title, price, quantity: 1, totalPrice: price });
+    }
+
+    cartCount += 1;
+    localStorage.setItem("cartCount", cartCount.toString());
+
     localStorage.setItem("cartItems", JSON.stringify(cart));
     console.log(cart);
     console.log("Your Item has been added to cart");
 
-    // Display the updated cart immediately after adding an item
     displayCart();
   });
 });
 
 function deleteItem(index) {
-  cart.splice(index, 1);
+  const item = cart[index];
+
+  if (item.quantity > 1) {
+    // If quantity is more than 1, decrease the quantity by 1
+    item.quantity -= 1;
+    item.totalPrice = item.quantity * item.price; // Recalculate total price
+  } else {
+    // If quantity is 1, remove the item completely from the cart
+    cart.splice(index, 1);
+  }
+
+  // Decrease cartCount when deleting an item
+  cartCount -= 1;
+  localStorage.setItem("cartCount", cartCount.toString());
+
   localStorage.setItem("cartItems", JSON.stringify(cart));
   displayCart();
 }
 
 function displayCart() {
-  cartCount.innerHTML = cart.length;
+  cartCountElement.innerHTML = cartCount;
+
   if (cart.length === 0) {
     cartItems.innerHTML = "Your cart is empty";
     cartTotal.innerHTML = "N " + 0 + " .00";
   } else {
-    let total = 0;
-    cartItems.innerHTML = cart.map((item, index) => {
-        let { title, price } = item;
-        total += parseFloat(price);
-        cartTotal.innerHTML = "N " + total.toFixed(2);
+    let total = cart.reduce((acc, item) => {
+      acc += item.totalPrice;
+      return acc;
+    }, 0);
+
+    cartItems.innerHTML = cart
+      .map((item, index) => {
+        let { title, quantity } = item;
         return `
           <div class="d-flex w-100 justify-content-between align-items-center bg-white p-2 my-2 rounded">
             <h4 class="fs-6 text-center">${title}</h4>
-            <h4 class="fs-6 text-center">N${price}</h4>
+            <h4 class="fs-6 text-center">${quantity}</h4>
             <button type="button" class="d-flex align-items-center justify-content-between border-0 btn btn-sm btn-danger" onclick="deleteItem(${index})"><i class="fa-solid fa-trash"></i></button>
           </div>`;
       })
       .join('');
+    cartTotal.innerHTML = `N ${total.toFixed(2)} .00`;
   }
 }
